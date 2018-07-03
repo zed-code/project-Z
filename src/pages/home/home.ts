cde305a77dadbe49861d92c3ae7deffedbc3b9c6
Aditya Agarwal <adiagrwl94@gmail.com>
2018-07-03 13:04:13 +0530

    Added sidebar for login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+
+  currentYear: any;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+  	const date = new Date();
+  	this.currentYear = date.getFullYear();
+  }
+
+  ngOnDestroy() {
+  	
   }
 
 }
